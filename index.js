eae27bd2b5d891f2d670fc51ed360b781b5234cb
Đỗ Thiên Giang <84732258+DoThienGiang@users.noreply.github.com>
2024-12-02 11:41:19 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const downloadPDF = ()=> {
     const link=document.createElement('a');
-    const filePath = './duc_cv.pdf'
+    const filePath = './giang_cv.pdf'
     link.href = filePath;
     link.download = filePath.substr(filePath.lastIndexOf('/') + 1);
     link.click();
